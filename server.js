7e73dc9d4ea2d543e80ef0358bd030576c32b261
victorkaiwong <victorkaihong@gmail.com>
2023-01-16 22:11:31 -0500

    updated server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Victor Kai Hong Wong - 156340200");
 });
 
 // setup http server to listen on HTTP_PORT
