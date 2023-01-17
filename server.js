5bbe7a8951672460a45b8f3a221c59195d79b27a
victorkaiwong <victorkaihong@gmail.com>
2023-01-16 22:14:07 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Victor Kai Hong Wong Student ID: 156340200 Date: 16-Jan-2023
+*
+*  Cyclic Web App URL: https://yellow-veil.cyclic.app/
+*
+*  GitHub Repository URL: https://github.com/VictorKwong/helloworld
+*
+********************************************************************************/ 
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
